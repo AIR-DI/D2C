6f7f2060e546809631f9f50ae2aa250afb2f231c
t6-thu <niuhy18@mails.tsinghua.edu.cn>
2023-01-06 23:48:50 +0800

    norm

--- example/benchmark/demo_h2o.py
+++ example/benchmark/demo_h2o.py
@@ -33,7 +33,7 @@ def main():
         'model.model_name': 'h2o',
         'train.data_loader_name': None,
         'train.device': device,
-        'train.seed': 52,
+        'train.seed': 19,
         'train.total_train_steps': 1000000,
         'train.batch_size': 128,
         'train.agent_ckpt_name': '1211'
@@ -51,7 +51,7 @@ def main():
     s_norm = dict(zip(['obs_shift', 'obs_scale'], real_dataset.state_shift_scale))
     data = real_dataset.data
 
-    real_env = benchmark_env(config=config)
+    real_env = benchmark_env(config=config, **s_norm)
     real_env_name = config.model_config.env.external.data_name
     if config.model_config.env.external.unreal_dynamics == "gravity":
         update_source_env_gravity(config.model_config.env.external.variety_degree, real_env_name)
@@ -61,7 +61,7 @@ def main():
         update_source_env_friction(config.model_config.env.external.variety_degree, real_env_name)
     else:
         raise RuntimeError("Got erroneous unreal dynamics %s" % config.model_config.env.external.unreal_dynamics)
-    sim_env = benchmark_env(config)
+    sim_env = benchmark_env(config, **s_norm)
     if config.model_config.env.external.unreal_dynamics == "gravity":
         update_source_env_gravity(1, real_env_name)
     elif config.model_config.env.external.unreal_dynamics == "density":
